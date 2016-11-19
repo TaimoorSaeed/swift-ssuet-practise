d3f1c774978c3b47fc265840a5b2071ea047f884
TaimoorSaeed <taimoor_saeed@hotmail.com>
2016-11-19 21:47:36 +0500

    var exe

---
+++ Variables Exercises.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: ## Variables Exercises
+//: In these exercises, you will create variables and constants, and you will handle simple compiler areas that arise when working with them.
+/*:
+### Exercise 1
+
+For each of the following names and types, create a variables or constant that best represents it:
+
+- boilingPointOfWaterInCelsius (Double)
+- gallonsRemainingInTank (Double)
+- naturalLogarithmicBase (Double)
+- ageOfSpecimenInWeeks (Int)
+- studentNickname (String)
+*/
+let boilingPointOfWaterInCelsius : Double
+var gallonsRemainingInTank: Double
+let naturalLogarithmicBase : Double
+var ageOfSpecimenInWeeks : Int
+var studentNickname : String
+//: ### Exercise 2
+//: Fix the following compiler errors related to assignment, variables, and constants:
+var favoriteLanguage : String = "Swift"
+gallonsRemainingInTank  = Double("2")!
+ageOfSpecimenInWeeks = Int(32.45)
+let _numberOfGasIcons: Int = Int(gallonsRemainingInTank)
+var numberOfPolygonsInModel = 30
+numberOfPolygonsInModel = 32
+var polygonsLeftToDraw = 0
+polygonsLeftToDraw = numberOfPolygonsInModel
+
+//: ### Exercise 3
+//: Fix the following compiler errors related to invalid names:
+var nameOfSchool = "Udacity"
+var _class = "Math"
+let _import = "Nissian"
+var 🐩myDog = "🐶"
+var 🐕yourDog = "🐶"
+
+
+
