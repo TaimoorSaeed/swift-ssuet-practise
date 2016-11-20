8027b4df03fddda9a5fd7b79e6163888f3f0dbf2
TaimoorSaeed <taimoor_saeed@hotmail.com>
2016-11-20 14:58:25 +0500

    strcuts

---
+++ Writing Structs with Properties.playground/Pages/Creating Variables from Struct Properties.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+/*:
+## Creating Variables from Struct Properties
+*/
+struct Student {
+    let name: String
+    var age: Int
+    var school: String
+}
+
+var ayush = Student(name: "Ayush Saraswat", age: 19, school: "Udacity")
+
+ayush.age = 20
+ayush.school = "USC"
+
+var collegeAged = ayush.age
+var anotherSchool = ayush.school
+
+ayush.name
+ayush.age
+//: [Next](@next)
